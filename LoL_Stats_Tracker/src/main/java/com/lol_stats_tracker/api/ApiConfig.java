b91b7eaf6ed3f03a9db4796553b5c4fd4ba22d31
unlmtddd <unlmtddd@gmail.com>
2026-01-10 16:02:46 +0100

    TestApi mit console

---
+++ LoL_Stats_Tracker/src/main/java/com/lol_stats_tracker/api/ApiConfig.java
@@ -0,0 +1,10 @@
+package com.lol_stats_tracker.api;
+
+public class ApiConfig
+{
+    public static  final  String API_KEY = "RGAPI-6c276e7a-93a2-41cf-a4ba-2e2189a4f6e9";
+    public static final String REGION = "europe";
+    public static final String PLATFORM  = "euw1";
+
+
+}
---
+++ LoL_Stats_Tracker/src/main/java/com/lol_stats_tracker/api/TestApi.java
@@ -0,0 +1,61 @@
+package com.lol_stats_tracker.api;
+import com.lol_stats_tracker.api.ApiConfig;
+import java.net.HttpURLConnection;
+import java.net.ResponseCache;
+import java.net.URL;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import static java.lang.System.out;
+
+
+public class TestApi
+{
+
+    public static void main ( String[] args)
+    {
+        String gameName = "Rekkles";
+        String tagline = "EUW";
+
+
+
+        try
+        {
+          String urlString = "https://" + ApiConfig.REGION + ".api.riotgames.com" + "/riot/account/v1/accounts/by-riot-id/" +
+                  gameName + "/" + tagline;
+
+          URL url  = new URL(urlString);
+          HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+          conn.setRequestMethod("GET");
+          conn.setRequestProperty("X-Riot-Token",ApiConfig.API_KEY);
+
+
+          int responseCode = conn.getResponseCode();
+          System.out.println("ResponseCode:" + responseCode);
+
+            if (responseCode == 200)
+            {
+                BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                String line;
+                StringBuilder response = new StringBuilder();
+
+                while ((line = in.readLine()) != null)
+                {
+                    response.append(line);
+                }
+                in.close();
+                System.out.println("Responce : " + response.toString());
+
+            }
+            else
+            {
+                System.out.println("Eror");
+            }
+
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
