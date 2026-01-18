89bdfd8df415f5b1c1279d4598afac2173947202
unlmtddd <unlmtddd@gmail.com>
2026-01-18 20:32:48 +0100

    api key

--- LoL_Stats_Tracker/src/main/java/com/lol_stats_tracker/api/ApiConfig.java
+++ LoL_Stats_Tracker/src/main/java/com/lol_stats_tracker/api/ApiConfig.java
@@ -2,7 +2,8 @@ package com.lol_stats_tracker.api;
 
 public class ApiConfig
 {
-    public static  final  String API_KEY = "RGAPI-36e810ee-16a4-443c-aaea-ec18d38d45f8";
+    public static  final  String API_KEY = "RGAPI-b04d8865-7266-4555-9d7b-1b1382a2e155";
     public static final String REGION = "europe";
     public static final String PLATFORM  = "euw1";
 }
+    
